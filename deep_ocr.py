7d5051abdd5ee2e86ed0456be54c5b98e96be3e5
Edward Pie <hackstockpie@gmail.com>
2018-10-21 22:57:08 +0200

    attempts to predict digits drawn with mouse

--- deep_ocr.py
+++ deep_ocr.py
@@ -2,15 +2,21 @@ import cv2
 import numpy as np
 from keras.models import model_from_json
 
-img = np.zeros((600,600), dtype="uint8")
-img[100:500,100:500] = 255
+file = open("model.json", "r")
+model_def = file.read()
+file.close()
+model = model_from_json(model_def)
+model.load_weights("model.h5")
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+img = np.zeros((600,600,3), dtype="uint8")
+img[100:500,100:500] = (255,255,255)
 pt1 = None
 pt2 = None
 is_drawing = False
 
 def line(img,p1,p2):
-    print("drawing")
-    cv2.line(img,p1,p2,0,3)
+    cv2.line(img,p1,p2,(255,0,0),10)
 
 def draw(event,x,y,flags,params):
     global pt1
@@ -40,5 +46,15 @@ while(True):
         is_drawing = True
     elif key == ord('c'):
         img[100:500,100:500] = (255,255,255)
+    elif key == ord('p'):
+        char_image = img[100:500,100:500]
+        char_image = cv2.cvtColor(char_image, cv2.COLOR_BGR2GRAY)
+        char_image = cv2.resize(char_image,(28,28))
+        input = char_image.reshape((28 * 28,))
+        input = input.astype('float32')
+        input /= 255
+        print(input.shape)
+        result = model.predict_proba(np.array([input]))
+        print("PREDICTION : ",result)
 
 cv2.destroyAllWindows()
